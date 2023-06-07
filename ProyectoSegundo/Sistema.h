8a6362aba95bfd5b6379c7bf2ba9522a3537e434
SIANNY BARRANTES BENAVIDES <sianny.barrantes.benavides@est.una.ac.cr>
2023-06-07 13:11:17 -0600

    .

--- ProyectoSegundo/Sistema.h
+++ ProyectoSegundo/Sistema.h
@@ -11,19 +11,17 @@ public:
 	virtual ~Sistema();
 
 	virtual string getID() = 0;
-	virtual string getNombre() = 0;
 	virtual string getCodigo() = 0;
+	virtual string getNombre() = 0;
 	virtual string getCaracteristica() = 0;
 	virtual double getPrecio() = 0;
 
-
+	virtual void setNombre(string) = 0;
 	virtual void setCodigo(string) = 0;
 	virtual void setCaracteristica(string) = 0;
 	virtual void setPrecio(double) = 0;
-	virtual void setNombre(string) = 0;
 
-
-	virtual void agregar(Componente*) = 0;
 	virtual string toString() = 0;
+	virtual void agregar(Componente*) = 0;
 	virtual void guardar(ostream&) = 0;
 };
