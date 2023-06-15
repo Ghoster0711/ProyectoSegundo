bbd0f373704791a374669162180503d9db7bc1f7
Joshua <82519856+Ghoster0711@users.noreply.github.com>
2023-06-15 04:02:14 -0600

    Encabezado a .h

--- ProyectoSegundo/Directo.h
+++ ProyectoSegundo/Directo.h
@@ -1,3 +1,14 @@
+/* --------------------------------------------------------------------
+*
+* EIF204 Programación 2
+* Proyecto 2
+*
+* 6 0483 0349 Joshua Yarit Amador Lara.
+* 40 264 0260 Sianny Barrantes Benavides.
+*
+* -------------------------------------------------------------------
+*/
+
 #pragma once
 #include "Factura.h"
 
